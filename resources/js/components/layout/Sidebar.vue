ad73048806594b63c7b3cbe761ddac216915a799
miglezub <miglezub@gmail.com>
2021-03-26 10:04:09 +0200

    add navigation and sidebar

---
+++ resources/js/components/layout/Sidebar.vue
@@ -0,0 +1,75 @@
+<template>
+    <nav id="sidebar" class="shrink">
+        <!-- <div class="sidebar-header">
+            <div class="header-text">
+                <h3>Bootstrap Sidebar</h3>
+                <strong>BS</strong>
+            </div>
+        </div> -->
+
+        <ul class="list-unstyled components">
+            <li class="active">
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <i class="fas fa-file-alt"></i>
+                    Tekstai
+                </a>
+                <ul class="collapse list-unstyled" id="homeSubmenu">
+                    <li>
+                        <a href="/texts">Tekstų sąrašas</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-briefcase"></i>
+                    About
+                </a>
+                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <i class="fas fa-copy"></i>
+                    Pages
+                </a>
+                <ul class="collapse list-unstyled" id="pageSubmenu">
+                    <li>
+                        <a href="#">Page 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-image"></i>
+                    Portfolio
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-question"></i>
+                    FAQ
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-paper-plane"></i>
+                    Contact
+                </a>
+            </li>
+        </ul>
+
+        <ul class="list-unstyled CTAs">
+            <li>
+                <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
+            </li>
+        </ul>
+    </nav>
+</template>
---
+++ resources/views/layouts/navigation_new.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg" id="top-navbar">
+  <!-- <a class="navbar-brand" href="/dashboard">
+        <x-application-logo class="block h-10 w-auto" />
+  </a> -->
+  <div id="sidebarCollapse" class="shrink"
+      onclick="document.getElementById('sidebar').classList.toggle('shrink');
+      document.getElementById('sidebarCollapse').classList.toggle('shrink');
+      document.getElementById('sidebarToggle').classList.toggle('fa-chevron-right');
+      document.getElementById('sidebarToggle').classList.toggle('fa-chevron-left');">
+      <i id="sidebarToggle" class="fas fa-lg fa-chevron-right"></i>
+  </div>
+  <div class="dropdown">
+    <a class="dropdown-toggle" href="#" role="button" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      {{ Auth::user()->name }}
+    </a>
+    <div class="dropdown-menu" aria-labelledby="userDropdown">
+      <form method="POST" action="{{ route('logout') }}">
+          @csrf
+          <a class="dropdown-item" href="route('logout')" 
+              onclick="event.preventDefault();this.closest('form').submit();">
+              {{ __('Log out') }}
+          </a>
+      </form>
+    </div>
+  </div>
+  <!-- <ul class="navbar-nav">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-chevron-down"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+    </div>
+  </ul> -->
+</nav>
