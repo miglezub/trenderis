218788c951a775d6ee1eabfa30030ba40ede245c
miglezub <miglezub@gmail.com>
2021-03-26 10:00:45 +0200

    add auth lithuanian translations

---
+++ resources/lang/lt/auth.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'            => 'Nurodyti blogi prisijungimo duomenys.',
+    'password'          => 'Neteisingas slaptažodis.',
+    'throttle'          => 'Per daug nesėkmingų prisijungimo bandymų. Prašome pabandyti po :seconds sekundžių.',
+    'email'             => 'El. paštas.',
+    'password1'         => 'Slaptažodis',
+    'remember_me'       => 'Prisiminti',
+    'log_in'            => 'Prisijungti',
+    'registration'      => 'Registracija',
+    'name'              => 'Slapyvardis',
+    'confirm_password'  => 'Patvirtinkite slaptažodį',
+    'register'          => 'Registruotis',
+    'login'             => 'Jau esate prisiregistravęs?',
+    'required'          => 'Prašome užpildyti šį lauką!',
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,14 +17,14 @@
 
             <!-- Email Address -->
             <div>
-                <x-label for="email" :value="__('Email')" />
+                <x-label for="email" :value="__('auth.email')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <x-label for="password" :value="__('auth.password1')" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
@@ -36,19 +36,23 @@
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('auth.remember_me') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
+                @if (0 and Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
 
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('auth.registration') }}
+                </a>
+
                 <x-button class="ml-3">
-                    {{ __('Log in') }}
+                    {{ __('auth.log_in') }}
                 </x-button>
             </div>
         </form>
