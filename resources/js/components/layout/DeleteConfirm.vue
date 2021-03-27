633b433bfb4a3ef909c75665bea6665fc1e9dc98
miglezub <miglezub@gmail.com>
2021-03-27 12:30:40 +0200

    show confirm modal on text delete

---
+++ resources/js/components/layout/DeleteConfirm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="modal" tabindex="-1" role="dialog" id="deleteModal">
+        {{ id }}
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" v-html="title"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p v-html="message"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" @click="approved()">Ištrinti</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Uždaryti</button>
+            </div>
+            </div>
+        </div>
+        </div>
+</template>
+<script>
+export default {
+    props: {
+        id: Number,
+        title: String,
+        message: String
+    },
+    methods: {
+        approved() {
+            this.$emit('approvedDeletion', this.id);
+        }
+    }
+}
+</script>
--- resources/js/components/text/AllTexts.vue
+++ resources/js/components/text/AllTexts.vue
@@ -3,7 +3,7 @@
         <h5 class="card-header">Tekstų sąrašas</h5>
         <div class="card-body">
             <div class="filter">
-                <form @submit.prevent="filterList">
+                <form @submit.prevent="filterList" class="d-inline">
                     <div class="form-group d-inline-block">
                         <label>Datos</label>
                         <date-picker 
@@ -14,6 +14,7 @@
                     </div>
                     <button type="submit" class="btn btn-primary d-inline-block">Filtruoti</button>
                 </form>
+                <router-link :to="{name: 'createText'}" class="btn btn-default float-right">Pridėti tekstą</router-link>
             </div>
     
             <table class="table">
@@ -27,29 +28,32 @@
                 <tbody>
                 <tr v-for="text in texts" :key="text.id">
                     <td>{{ text.id }}</td>
-                    <td>{{ text.original_text }}</td>
+                    <td v-html="text.original_text"></td>
                     <td>
                         <div class="btn-group" role="group">
                             <router-link :to="{name: 'edit', params: { id: text.id }}" class="btn btn-default">Redaguoti</router-link>
-                            <button class="btn btn-danger" @click="deleteProduct(text.id)">Ištrinti</button>
+                            <button class="btn btn-danger" @click="triggerDelete(text.id)">Ištrinti</button>
                         </div>
                     </td>
                 </tr>
                 </tbody>
             </table>
         </div>
+        <delete-confirm title="Teksto ištrynimas" message="Ar tikrai norite ištrinti tekstą?<br>Pašalinus tekstą bus ištrinti ir jo analizės rezultatai." :id="deleteId" v-on:approvedDeletion="deleteText"></delete-confirm>
     </div>
 </template>
  
 <script>
     import DatePicker from 'vue2-datepicker';
     import 'vue2-datepicker/index.css';
+    import DeleteConfirm from '../layout/DeleteConfirm.vue';
     export default {
-        components: { DatePicker },
+        components: { DatePicker, DeleteConfirm },
         data() {
             return {
                 texts: [],
-                date: []
+                date: [],
+                deleteId: 0
             }
         },
         created() {
@@ -60,12 +64,17 @@
                 });
         },
         methods: {
+            triggerDelete(id) {
+                this.deleteId = id;
+                $('#deleteModal').modal('show');
+            },
             deleteText(id) { 
                 this.axios
                     .delete(`/api/texts/${id}`)
                     .then(response => {
                         let i = this.texts.map(data => data.id).indexOf(id);
-                        this.texts.splice(i, 1)
+                        this.texts.splice(i, 1);
+                        $('#deleteModal').modal('hide');
                     });
             },
             filterList() {
