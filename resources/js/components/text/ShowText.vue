808eb9757976e6620d865d407100009af7c6b941
miglezub <miglezub@gmail.com>
2021-03-29 21:00:59 +0300

    create show text component

---
+++ resources/js/components/text/ShowText.vue
@@ -0,0 +1,166 @@
+<template>
+    <div class="card">
+        <h4 class="card-header">
+            Teksto analizės rezultatai
+            <router-link :to="{name: 'texts'}" class="btn btn-danger float-right">Atgal</router-link>
+        </h4>
+        <div class="card-body row">
+            <div class="col-md-6">
+                <div class="jumbotron" v-html="text.original_text">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <b-button class="btn btn-primary float-right mb-2" @click="showModal()">Atnaujinti analizę</b-button>
+                <b-table
+                    id="analysis-table"
+                    striped hover
+                    :items="results"
+                    :fields="fields"
+                    sticky-header='750px'
+                    style="clear: both; height: 750px;"
+                    :busy.sync="isBusy"
+                    >
+                    <template v-slot:cell(original_text)="data">
+                        <div v-html="data.item.original_text"></div>
+                    </template>
+                    <template v-slot:table-busy>
+                        <div class="text-center text-orange my-5">
+                                <b-spinner class="align-middle" type="grow"></b-spinner>
+                                <strong>Kraunasi...</strong>
+                        </div>
+                    </template>
+                </b-table>
+            </div>
+        </div>
+        <div class="modal" tabindex="-1" role="dialog" id="analyseModal">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Analizės nustatymai</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="language-select" class="font-weight-bold">Teksto kalba</label>
+                        <select class="custom-select" id="language-select" v-model="text.language_id">
+                            <option :value="language.id" v-for="language in languages" :key="language.id">{{ language.name }}</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label class="switch">
+                            <input type="checkbox" v-model="text.use_idf">
+                            <span class="slider round"></span>
+                        </label>
+                        <span class="pl-2">Ar naudoti TF-IDF?</span>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-success" @click="analyse()">Analizuoti</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Uždaryti</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                text: {},
+                results: {},
+                languages: {},
+                limit: 100,
+                isBusy: true,
+                fields: [
+                    {
+                        key: 'key',
+                        label: 'Žodis',
+                        sortable: true,
+                    },
+                    {
+                        key: 'value.freq',
+                        label: 'Pasikartojimai',
+                        sortable: true,
+                    },
+                    {
+                        key: 'value.tf',
+                        label: 'TF',
+                        sortable: true,
+                        formatter: value => {
+                            return value.toFixed(4);
+                        }
+                    },
+                ],
+            }
+        },
+        created() {
+            this.fetchData();
+        },
+        methods: {
+            fetchData() {
+                this.isBusy = true;
+                if(!this.$route || !this.$route.params.id) {
+                    this.$router.push({ name: 'texts' });
+                } else {
+                    this.axios
+                        .get(`/api/texts/${this.$route.params.id}`)
+                        .then((res) => {
+                            this.text = res.data.text;
+                            if(res.data.results) {
+                                this.results = Object.entries(res.data.results).map(([key, value]) => ({key,value}));
+                                console.log(this.results);
+                                if(this.text.use_idf) {
+                                    var field = {
+                                        key: 'value.idf',
+                                        label: 'IDF',
+                                        sortable: true,
+                                        formatter: value => {
+                                            return value.toFixed(4);
+                                        }
+                                    };
+                                    this.fields.push(field);
+                                    var field = {
+                                        key: 'value.tf-idf',
+                                        label: 'TF-IDF',
+                                        sortable: true,
+                                        formatter: value => {
+                                            return value.toFixed(4);
+                                        }
+                                    };
+                                    this.fields.push(field);
+                                }
+                            }
+                            this.loadLanguages();
+                            this.isBusy = false;
+                        });
+                }
+            },
+            loadLanguages() {
+                this.axios
+                    .get('/api/languages')
+                    .then(response => {
+                        this.languages = response.data;
+                    });
+            },
+            analyse() {
+                var that = this;
+                this.axios
+                    .post(`/api/analyse/${this.$route.params.id}`, this.text)
+                    .then(function(response) {
+                        if(response.error) {
+                            console.log(response.error);
+                        } else {
+                            that.fetchData();
+                            $('#analyseModal').modal('hide');
+                        }
+                    });
+            },
+            showModal() {
+                $('#analyseModal').modal('show');
+            }
+        }
+    }
+</script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,5 +1,7 @@
 import AllTexts from './components/text/AllTexts.vue';
 import AddText from './components/text/AddText.vue';
+import ShowText from './components/text/ShowText.vue';
+
 import PageNotFound from './components/PageNotFound.vue';
  
 export const routes = [
@@ -17,6 +19,11 @@ export const routes = [
         name: 'createText',
         path: '/text/create',
         component: AddText
+    },
+    {
+        name: 'showText',
+        path: '/text/show',
+        component: ShowText
     },
     { path: "*", component: PageNotFound }
 ];
