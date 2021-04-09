b600b1ce1ea14092c9d6b80f835a0c4cf8ddbae4
miglezub <miglezub@gmail.com>
2021-04-09 22:33:19 +0300

    add sorting comparator, improve speed of analyse function

--- app/Http/Controllers/TextAnalysisController.php
+++ app/Http/Controllers/TextAnalysisController.php
@@ -7,8 +7,30 @@ use App\Models\User;
 
 class TextAnalysisController extends Controller
 {
+    static function cmp($a, $b)
+    {
+        if (key_exists("tfidf", $a) && !empty($a['tfidf'])) {
+            if($a['tfidf'] == $b['tfidf']) {
+                if ($a['freq'] == $b['freq']){
+                    return (key($a) < key($b)) ? -1 : 1;
+                }
+                return ($a['freq'] < $b['freq']) ? 1 : -1;
+            } else {
+                return (($a["tfidf"]-$b["tfidf"]) < 0) ? 1 : -1;
+            }
+        } else if (key_exists("freq", $a) && !empty($a['freq'])) {
+            if ($a['freq'] == $b['freq']){
+                return 0;
+            }
+            return ($a['freq'] < $b['freq']) ? 1 : -1;
+        } else {
+            return 0;
+        }
+    }
+
     public function analyse($id, $user_id)
     {
+        $start = microtime(true);
         $analysis = TextAnalysis::find($id);
         $text = $analysis->text;
         $tokenizer = new TokenizerCustom($text->original_text, $text->language_id);
@@ -20,10 +42,16 @@ class TextAnalysisController extends Controller
         $user = User::find($user_id);
         $total_documents = $user->texts()->count();
         foreach($results as $key => $result) {
-            $results[$key] = array();
-            $results[$key]['freq'] = $result;
-            $results[$key]['tf'] = $result/$total;
+            if(!is_array($results[$key])) {
+                $results[$key] = array();
+                $results[$key]['freq'] = $result;
+                $results[$key]['tf'] = $result/$total;
+            } else {
+                $results[$key]['freq'] += $result;
+                $results[$key]['tf'] += $result/$total;
+            }
             $results[$key]['lemma'] = $lemmatizer->lemmatize($key);
+            $results[$key]['w'] = $key;
             if($text->use_idf) {
                 $search = '%' . $key . '%';
                 $search2 = '"' . $key . '"';
@@ -41,20 +69,30 @@ class TextAnalysisController extends Controller
                 } else {
                     $results[$key]['idf'] = 1;
                 }
-                $results[$key]['tf-idf'] = $results[$key]['tf'] * $results[$key]['idf'];
+                $results[$key]['tfidf'] = $results[$key]['tf'] * $results[$key]['idf'];
             }
-        }
-        foreach($results as $key => $result) {
             if(key_exists('lemma', $results[$key]) && $results[$key]['lemma'] && key_exists($results[$key]['lemma'], $results)) {
-                $results[$results[$key]['lemma']]['freq'] += $results[$key]['freq'];
-                $results[$results[$key]['lemma']]['tf'] += $results[$key]['tf'];
+                if(!is_array($results[$results[$key]['lemma']])) {
+                    $results[$results[$key]['lemma']] = array();
+                    $results[$results[$key]['lemma']]['freq'] = $results[$key]['freq'];
+                    $results[$results[$key]['lemma']]['tf'] = $results[$key]['tf'];
+                } else {
+                    $results[$results[$key]['lemma']]['freq'] += $results[$key]['freq'];
+                    $results[$results[$key]['lemma']]['tf'] += $results[$key]['tf'];
+                }
                 if($text->use_idf) {
                     // $results[$results[$key]['lemma']]['idf'] += $results[$key]['idf'];
-                    $results[$results[$key]['lemma']]['tf-idf'] = $results[$results[$key]['lemma']]['tf'] * $results[$results[$key]['lemma']]['idf'];
+                    if(!key_exists('idf', $results[$results[$key]['lemma']])) {
+                        $results[$results[$key]['lemma']]['idf'] = 1;
+                    }
+                    $results[$results[$key]['lemma']]['tfidf'] = $results[$results[$key]['lemma']]['tf'] * $results[$results[$key]['lemma']]['idf'];
                 }
                 $results[$results[$key]['lemma']]['incl'][] = $key;
+                unset($results[$key]);
             }
         }
-        $analysis->update(['results' => json_encode($results, JSON_INVALID_UTF8_IGNORE)]);
+        usort($results, array($this, "cmp"));
+        $end = microtime(true);
+        $analysis->update(['results' => json_encode($results, JSON_INVALID_UTF8_IGNORE), 'duration' => $end - $start]);
     }
 }
