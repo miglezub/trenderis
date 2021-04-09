0fef5172242cf20b805db96f4fb630af1da4ca7e
miglezub <miglezub@gmail.com>
2021-04-09 22:34:46 +0300

    add import from botis, analysis recalculation function

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\TextAnalysis;
 use Illuminate\Http\Request;
 
 class TextController extends Controller
@@ -55,7 +57,9 @@ class TextController extends Controller
             $text = $user->texts()->find($id);
             $text['original_text'] = nl2br($text['original_text']);
             $analysis = $text->text_analysis->last();
-            $json['results'] = json_decode($analysis->results);
+            if($analysis) {
+                $json['results'] = json_decode($analysis->results);
+            }
             $json['text'] = $text;
             return response()->json($json);
         } else {
@@ -163,5 +167,104 @@ class TextController extends Controller
     {
         $lemmatizer = new Lemmatizer(1);
         return response()->json($lemmatizer->formEndings());
+    }
+
+    public function import()
+    {
+        set_time_limit(150);
+        $user = \App\Models\User::find(1);
+        $ch = curl_init();
+
+        $page = 1;
+        curl_setopt($ch, CURLOPT_URL, config('constants.botis_url') . "&page=" . $page);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);
+
+        $json = json_decode($output, true);
+        foreach($json as $text) {
+            $newText = $user->texts()->create([
+                'original_text' => html_entity_decode($text['description']),
+                'title' => html_entity_decode($text['title']),
+                'language_id' => 1,
+                'use_idf' => 1,
+                'use_word2vec' => 0,
+                'created_at' => $text['created_at']
+            ]);
+    
+            if($newText) {
+                $analysis = $newText->text_analysis()->create([
+                    'lemmatized_text' => '',
+                    'results' => '',
+                    'use_idf' => 1,
+                    'use_word2vec' => 0
+                ]);
+                $analysisController = new TextAnalysisController();
+                $analysisController->analyse($analysis->id, auth()->user()->id);
+            }
+        }
+    }
+
+    public function dayResults()
+    {
+        $start = microtime(true);
+        $user = \App\Models\User::find(1);
+        $texts = $user->texts()->get()->toArray();
+        $results = array();
+        foreach($texts as $text) {
+            $analysis = TextAnalysis::where('text_id', '=', $text['id'])->orderBy('updated_at')->take(1)->get()->last();
+            if($analysis) {
+                foreach(json_decode($analysis->results) as $a) {
+                    if(!key_exists($a->w, $results)) {
+                        $results[$a->w] = array();
+                        $results[$a->w]['freq'] = $a->freq;
+                        $results[$a->w]['tf'] = $a->tf;
+                        if(isset($a->tfidf)) {
+                            if(key_exists('tf-idf', $results[$a->w])) {
+                                $results[$a->w]['tf-idf'] += $a->tfidf;
+                            } else {
+                                $results[$a->w]['tf-idf'] = $a->tfidf;
+                            }
+                        }
+                    } else {
+                        $results[$a->w]['freq'] += $a->freq;
+                        $results[$a->w]['tf'] += $a->tf;
+                        if(!key_exists('tf-idf', $results[$a->w])) {
+                            $results[$a->w]['tf-idf'] = 0;
+                        }
+                        if(isset($a->tfidf)) {
+                            $results[$a->w]['tf-idf'] += $a->tfidf;
+                        } else {
+                            $results[$a->w]['tf-idf'] += $a->tf;
+                        }
+                    }
+                }
+            }
+        }
+        $res = array();
+        foreach($results as $key => $result) {
+            $results[$key]['w'] = $key;
+            $res[] = $results[$key];
+        }
+        usort($results, array(TextAnalysisController::class, "cmp"));
+        $results = array_splice($results, 0, 20, true);
+        $end = microtime(true);
+        return response()->json(array('results' => $results, 'time' => $end - $start));
+    }
+
+    public function recalculate()
+    {
+        set_time_limit(150);
+        $texts = \App\Models\Text::all();
+        foreach($texts as $text) {
+            $analysis = $text->text_analysis()->create([
+                'lemmatized_text' => '',
+                'results' => '',
+                'use_idf' => 1,
+                'use_word2vec' => $text->use_word2vec
+            ]);
+            $analysisController = new TextAnalysisController();
+            $analysisController->analyse($analysis->id, $text->user_id);
+        }
     }
 }
