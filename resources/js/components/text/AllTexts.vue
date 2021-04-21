a1e37fc547f712c97276e4a69cf8859103a16349
miglezub <miglezub@gmail.com>
2021-04-21 12:06:00 +0300

    add filter error message, fix pagination when using filter

--- resources/js/components/text/AllTexts.vue
+++ resources/js/components/text/AllTexts.vue
@@ -3,8 +3,8 @@
         <h5 class="card-header">Tekstų sąrašas</h5>
         <div class="card-body">
             <div class="filter">
-                <form @submit.prevent="filterList" class="d-inline">
-                    <div class="form-group d-inline-block">
+                <form @submit.prevent="submitFilter" class="d-inline">
+                    <div class="form-group">
                         <label class="font-weight-bold">Datos</label>
                         <date-picker 
                             v-model="date" 
@@ -15,11 +15,11 @@
                             :disabled-date="disabledTomorrowAndLater">
                         </date-picker>
                     </div>
-                    <div class="form-group d-inline-block">
+                    <div class="form-group">
                         <label class="font-weight-bold">Raktažodis</label>
                         <input type="text" class="form-control" placeholder="Įveskite raktažodį" v-model="keyword">
                     </div>
-                    <div class="form-group  d-inline-block">
+                    <div class="form-group">
                         <label for="key-select" class="font-weight-bold">API raktas</label>
                         <select class="custom-select" id="key-select" v-model="key">
                             <option value="" disabled>Pasirinkite API raktą</option>
@@ -27,6 +27,7 @@
                         </select>
                     </div>
                     <button type="submit" class="btn btn-primary d-inline-block">Filtruoti</button>
+                    <button type="button" class="btn btn-danger" title="Išvalyti filtrą" @click="clearFilter"><i class="fas fa-trash-alt"></i></button>
                 </form>
                 <router-link :to="{name: 'createText'}" class="btn btn-success float-right">Pridėti tekstą</router-link>
             </div>
@@ -47,7 +48,7 @@
                     <div v-html="data.item.title"></div>
                 </template>
                 <template v-slot:cell(top_results)="data">
-                    <div v-html="data.item.top_results.join(', ')"></div>
+                    <div v-if="data.item.top_results" v-html="data.item.top_results.join(', ')"></div>
                 </template>
                 <template v-slot:cell(buttons)="data">
                     <b-button-group class="float-right">
@@ -68,10 +69,11 @@
                 :per-page="perPage"
                 aria-controls="texts-table"
                 align="right"
-                v-if="totalTexts > 10"
+                v-if="totalTexts > 5"
                 ></b-pagination>
         </div>
         <delete-confirm title="Teksto ištrynimas" message="Ar tikrai norite ištrinti tekstą?<br>Pašalinus tekstą bus ištrinti ir jo analizės rezultatai." :id="deleteId" v-on:approvedDeletion="deleteText"></delete-confirm>
+        <message id="filterMessage" type="error" title="Filtravimo klaida" message="Prašome suvesti bent vieną filtro lauką!"></message>
     </div>
 </template>
  
@@ -80,8 +82,9 @@
     import 'vue2-datepicker/index.css';
     import 'vue2-datepicker/locale/lt';
     import DeleteConfirm from '../layout/DeleteConfirm.vue';
+    import Message from '../layout/Message.vue';
     export default {
-        components: { DatePicker, DeleteConfirm },
+        components: { DatePicker, DeleteConfirm, Message },
         data() {
             return {
                 texts: [],
@@ -135,14 +138,9 @@
             }
         },
         mounted() {
-            this.axios
-                .get('/api/texts')
-                .then(response => {
-                    this.texts = response.data.results;
-                    this.totalTexts = response.data.total;
-                    this.loadLanguages();
-                    this.fetchApiKeys();
-                });
+            this.fetchData();
+            this.loadLanguages();
+            this.fetchApiKeys();
         },
         computed: {
             rows() {
@@ -158,16 +156,36 @@
         },
         methods: {
             async fetchData() {
+                this.busy = true;
+                var date1 = "";
+                var date2 = "";
+                if(this.date[0]) {
+                    var date1 = new Date();
+                    date1.setTime(this.date[0].getTime() + 3600 * 1000 * 3 - 1);
+                }
+                if(this.date[1]) {
+                    var date2 = new Date();
+                    date2.setTime(this.date[1].getTime() + 3600 * 1000 * 27 - 1);
+                }
                 this.axios
-                .get(`/api/texts?page=${this.currentPage}`)
+                .get('/api/texts/filter', {
+                    params: {
+                        date1: date1,
+                        date2: date2,
+                        keyword: this.keyword,
+                        key: this.key,
+                        page: this.currentPage,
+                        limit: this.perPage
+                    }
+                })
                 .then(response => {
                     this.texts = response.data.results;
                     this.totalTexts = response.data.total;
+                    this.busy = false;
                 });
             },
             triggerDelete(id) {
                 this.deleteId = id;
-                $('#deleteModal').modal('show');
             },
             deleteText(id) { 
                 this.axios
@@ -176,7 +194,27 @@
                         let i = this.texts.map(data => data.id).indexOf(id);
                         this.texts.splice(i, 1);
                         $('#deleteModal').modal('hide');
+                        $('#textDeleted').modal('show');
                     });
+            },
+            submitFilter() {
+                $('form input, form select').removeClass('is-invalid');
+                if(this.keyword == "" && this.key == "" && (this.date.length < 2 || this.date[0] == "" || this.date[1] == "")) {
+                    $('form input, form select').addClass('is-invalid');
+                    $('#filterMessage').modal('show');
+                    return;
+                } else {
+                    this.filterList();
+                }
+            },
+            clearFilter() {
+                $('form input, form select').removeClass('is-invalid');
+                this.date[0] = "";
+                this.date[1] = "";
+                this.keyword = "";
+                this.key = "";
+                this.page = 1;
+                this.fetchData();
             },
             filterList() {
                 this.isBusy = true;
@@ -184,23 +222,27 @@
                 var date2 = "";
                 if(this.date[0]) {
                     var date1 = new Date();
-                    date1.setTime(this.date[0].getTime() + 3600 * 1000 * 26 - 1);
+                    date1.setTime(this.date[0].getTime() + 3600 * 1000 * 3 - 1);
                 }
                 if(this.date[1]) {
                     var date2 = new Date();
-                    date2.setTime(this.date[1].getTime() + 3600 * 1000 * 26 - 1);
+                    date2.setTime(this.date[1].getTime() + 3600 * 1000 * 27 - 1);
                 }
+                this.currentPage = 1;
                 this.axios
                     .get('/api/texts/filter', {
                         params: {
                             date1: date1,
                             date2: date2,
                             keyword: this.keyword,
-                            key: this.key
+                            key: this.key,
+                            page: this.currentPage,
+                            limit: this.perPage
                         }
                     })
                     .then(response => {
-                        this.texts = response.data;
+                        this.texts = response.data.results;
+                        this.totalTexts = response.data.total;
                         this.isBusy = false;
                     });
             },
